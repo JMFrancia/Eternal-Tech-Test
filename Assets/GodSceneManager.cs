7ae0d0c8d65e3472119343e61786086688aaff7e
JMFrancia <JMFrancia86@gmail.com>
2020-09-27 13:14:47 -0400

    Set up god scene

---
+++ Assets/GodSceneManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodSceneManager : MonoBehaviour
+{
+    [SerializeField] GameObject fakePlayer;
+    [SerializeField] Transform entryPoint;
+    [SerializeField] Transform heightPoint;
+}
