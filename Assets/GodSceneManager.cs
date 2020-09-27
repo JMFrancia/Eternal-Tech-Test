e9e7da37213e496d5e7550f5800e2150ea97478a
JMFrancia <JMFrancia86@gmail.com>
2020-09-27 13:56:54 -0400

    First draft entry scene

--- Assets/GodSceneManager.cs
+++ Assets/GodSceneManager.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class GodSceneManager : MonoBehaviour
 {
     [SerializeField] GameObject fakePlayer;
-    [SerializeField] Transform entryPoint;
-    [SerializeField] Transform heightPoint;
+    [SerializeField] Transform godStartingPos;
+    [SerializeField] Transform godFinalPos;
+    [SerializeField] Camera godCam;
+
+    [Header("Entry scene")]
+    [SerializeField] Camera entryCam;
+    [SerializeField] Transform entryPosition;
+    [SerializeField] float entryShakeDist = .5f;
+    [SerializeField] int shakeFreq = 3;
+
+    int shakeCounter = 0;
+    bool shakeEffect = true;
+
+    private void Update()
+    {
+        fakePlayer.transform.position = entryPosition.position;
+        if (shakeEffect && ++shakeCounter % shakeFreq == 0)
+        {
+            fakePlayer.transform.position += Random.insideUnitSphere * entryShakeDist;
+        }
+    }
+
 }
